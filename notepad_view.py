716c053cb9cd143e757464bd75b0710cc9a7db13
Amine Asli <asli.amine@gmail.com>
2020-01-10 15:28:01 +0100

    Create bind_keyboard_events instance method for binding keyboard events to document_area

--- notepad_view.py
+++ notepad_view.py
@@ -18,10 +18,20 @@ class NotepadView:
         self.document_area.pack(expand='yes', fill='both')
 
         x_scroll_bar = Scrollbar(self.document_area, orient='horizontal')
-        y_scroll_bar = Scrollbar(self.document_area,)
+        y_scroll_bar = Scrollbar(self.document_area)
         self.document_area.configure(yscrollcommand=y_scroll_bar.set, xscrollcommand=x_scroll_bar.set)
-        x_scroll_bar.config(command=self.document_area.yview)
+        x_scroll_bar.config(command=self.document_area.xview)
         x_scroll_bar.pack(side='bottom', fill='x')
         y_scroll_bar.config(command=self.document_area.yview)
         y_scroll_bar.pack(side='right', fill='y')
         
+        self.bind_keyboard_events()
+
+    def select_all(self, event=None):
+        self.document_area.tag_add('sel', '1.0', 'end')
+        return "break"
+    
+    def bind_keyboard_events(self):
+        self.document_area.bind('<Control-A>', self.select_all)
+        self.document_area.bind('<Control-a>', self.select_all)
+
