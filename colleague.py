1dbf810fa02f61767b3d47bf94a7d08948bfe41a
Amine Asli <asli.amine@gmail.com>
2020-01-13 16:50:26 +0100

    Add class colleague for storing mediator's instance inside components objects

---
+++ colleague.py
@@ -0,0 +1,17 @@
+
+class Colleague:
+    """
+    provides the basic functionality of storing a mediator's
+    instance inside component objects.
+    """
+
+    def __init__(self, mediator = None):
+        self._mediator = mediator
+
+    @property
+    def mediator(self):
+        return self._mediator
+
+    @mediator.setter
+    def mediator(self, mediator):
+        self._mediator = mediator
